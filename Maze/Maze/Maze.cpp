c599e83f6ec3ac0a34268c6a2dfdaaa6e5edc4b1
dani dimov <gkhrisimov19@codingburgas.bg>
2021-10-26 17:35:56 +0300

    Start the project

---
+++ Maze/Maze/Maze.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPlaying = true;
+
+void Menu()
+{
+	cout << "1. Play" << endl;
+	cout << "2. Rules" << endl;
+	cout << "3. Information" << endl;
+}
+
+void Options(int choise)
+{
+	if (choise == 1)
+	{
+		cout << "You are playing a game" << endl;
+		
+	}
+	else if (choise == 2)
+	{
+		cout << "Those are the rules" << endl;
+	}
+	else if (choise == 3)
+	{
+		cout << "information" << endl;
+	}
+	else
+	{
+		cout << "incorrect input. Try again" << endl;
+		Options(choise);
+	}
+}
+
+void StopPlaying(string answer)
+{
+	cout << "Do you want to spot exit the game." << endl;
+	cout << "Yes/No" << endl;
+	cin >> answer;
+	if (answer == "yes" or answer == "Yes")
+	{
+		isPlaying = false;
+	}
+}
+
+int main()
+{
+	int n;
+	string choise;
+	for (;isPlaying;)
+	{
+		Menu();	
+		cin >> n;
+		Options(n);
+		StopPlaying(choise);
+	}
+
+}
+
